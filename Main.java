21ca83732e65d4606743ef103577f16370ab048b
lanaghali327-coder <lanaghali327@gmail.com>
2025-11-14 15:13:25 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package zuj.edu.jo ;
+import java.util.Scanner;
+puplic class Text3 {
+puplic static void main (String [] arge )  {
+Scanner s="new Scanner (System.in );
+  System.out.println("Eneter n1:");
+  int n1=s.nextInt();
+  System.out.print("Enter n2 :");
+  int n2=s.nextInt(); 
+
+  if(n1>n2 || n1==n2){
+  System.out.println(n1*2);
+  }
+  eles {
+  System.out.println(n2*2); 
+  }
